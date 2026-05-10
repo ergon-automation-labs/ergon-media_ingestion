e074e9c50cc8900978fda0200891330778f80c53
Abby Malson <august.malson@gmail.com>
2026-05-10 00:55:06 -0600

    Release v0.1.7.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BotArmyMediaIngestion.MixProject do
   def project do
     [
       app: :bot_army_media_ingestion,
-      version: "0.1.6",
+      version: "0.1.7",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
