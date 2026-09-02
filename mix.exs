604ebb49c09faf5f7c83fbb1114b680c89a8b15f
Abby Malson <august.malson@gmail.com>
2026-09-02 09:20:38 +0300

    chore: Bump version to 0.1.31 to pick up runtime v0.14.66

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BotArmyMediaIngestion.MixProject do
   def project do
     [
       app: :bot_army_media_ingestion,
-      version: "0.1.30",
+      version: "0.1.31",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
